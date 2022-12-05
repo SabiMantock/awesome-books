0f23369f71a966d0a512efb6a6fddb2d5cc39cb6
Sabi Mantock <sabimantock@gmail.com>
2022-12-05 18:04:06 +0000

    Created a function to add books to the collection

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable quotes */
 const collection = document.getElementById("collection");
-const books = [{title:"Twilight", author:"Delora Beau"},{title:"Twilight", author:"Delora Beau"}];
-books.map((item)=>{
-    collection.innerHTML += `
-    <li>
-<p>
-${item.title}
-</p>
-<p>
-${item.author}
-</p>
-<button>
-Remove
-</button>
+const title = document.getElementById("book_title");
+const author = document.getElementById("book_author");
+const form = document.getElementById("form");
 
+const books = [];
+
+const addBook = (book) => {
+  collection.innerHTML += `
+    <li id="${book.id}">
+      <p>${book.title}</p>
+      <p>${book.author}</p>
+      <button type='button'>Remove</button>
     </li>
-    
-    
-    `
-})
+`;
+};
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (title.value && author.value !== "") {
+    const book = {
+      id: Math.random(),
+      title: title.value,
+      author: author.value,
+    };
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    addBook(book);
+    form.reset();
+    console.log(books);
+  }
+});
