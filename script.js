9fc1f5bf7925da393f3997a252de6a2b52e853e3
Sabi Mantock <sabimantock@gmail.com>
2022-12-07 08:29:51 +0000

    Modified addBook function to check if the array is empty then it should populate it.

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable quotes */
-const collection = document.getElementById("collection");
-const title = document.getElementById("book_title");
-const author = document.getElementById("book_author");
-const form = document.getElementById("form");
+const collection = document.getElementById('collection');
+const title = document.getElementById('book_title');
+const author = document.getElementById('book_author');
+const form = document.getElementById('form');
 
-const books = [];
+let books = [];
 
 const addBook = (book) => {
   collection.innerHTML += `
-    <li id="${book.id}">
+    <li id='${book.id}'>
       <p>${book.title}</p>
       <p>${book.author}</p>
-      <button class='delete' type='button' onclick=remove_book("${book.id}") >Remove</button>
+      <button id='delete' type='button' onclick = 'removeBook(${book.id})'>Remove</button>
     </li>
 `;
 };
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (title.value && author.value !== "") {
+  if (title.value && author.value !== '') {
     const book = {
       id: Math.random(),
       title: title.value,
       author: author.value,
     };
+
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     addBook(book);
     form.reset();
-    console.log(books);
   }
 });
 
-function remove_book(book_id){
-    console.log(book_id)
-    let deleteBook = JSON.parse(localStorage.getItem("books"))
-    let newArray = deleteBook.filter((book) => book.id != book_id)
-    console.log(newArray)
-    localStorage.setItem("books", JSON.stringify(newArray));
+function removeBook(bookId) {
+  const deleteBook = JSON.parse(localStorage.getItem('books'));
 
+  const newArray = deleteBook.filter((book) => book.id !== bookId);
+  localStorage.setItem('books', JSON.stringify(newArray));
+
+  const booksId = document.getElementById(bookId);
+  booksId.remove();
 }
+
+removeBook();
